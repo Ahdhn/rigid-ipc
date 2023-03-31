13e1235b34a0cae349a8b099dfa8b7bfd2f45d2e
Ahmed Mahmoud <ahmed.mahmoud@autodesk.com>
2023-03-31 14:00:13 -0400

    one more fix for Ubuntu

--- cmake/recipes/ipc_toolkit.cmake
+++ cmake/recipes/ipc_toolkit.cmake
@@ -8,7 +8,7 @@ include(FetchContent)
 FetchContent_Declare(
     ipc_toolkit    
 	GIT_REPOSITORY https://github.com/Ahdhn/ipc-toolkit.git
-	GIT_TAG 6e377ef470a1a57188af6f8775681cf9430061af    
+	GIT_TAG ad65f0b250a6f2e77f9d8af0289afbee855cd0b5    
 	#GIT_REPOSITORY https://github.com/ipc-sim/ipc-toolkit.git
 	#GIT_TAG d41d6ca93cf9b9c4c01a3177fbb68b63c6b74df1
     GIT_SHALLOW FALSE
